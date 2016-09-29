fd15885cd7d6bd252df5cffcd5ccd1fe76e7912e
towry <tovvry@gmail.com>
2016-09-29 11:57:25 +0800

    Fix lint error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 'use strict';
 
-var exports = module.exports = DepsPlugin;
+module.exports = DepsPlugin;
 var pkg = require('./package.json');
 
 
@@ -15,32 +15,33 @@ var err = '`' + pkg.name  + '` requires a callback';
  * about the files that being required.
  */
 function DepsPlugin (callback) {
-  if (!callback || typeof callback !== 'function') {
-    throw new Error(err);
-  }
+	if (!callback || typeof callback !== 'function') {
+		throw new Error(err);
+	}
 
-  this.callback = callback;
+	this.callback = callback;
 }
 
 DepsPlugin.prototype.apply = function (compiler) {
-  var options = compiler.options;
-  var noBail = false;
+	var options = compiler.options;
+	var noBail = false;
 
-  if (!options.bail) {
-    noBail = true;
-  }
-  
-  if (noBail) {
-    compiler.plugin('this-compilation', function (compilation) {
-      compilation.plugin('additional-assets', function (cb) {
-        this.callback(compilation.fileDependencies || [], compiler.options);
-        process.exit(0);
-      }.bind(this));
-    }.bind(this));
-  }
+	if (!options.bail) {
+		noBail = true;
+	}
 
-  compiler.plugin('emit', function (compilation, compileCallback) {
-    this.callback(compilation.fileDependencies || [], compiler.options);
-    compileCallback();
-  }.bind(this));
+	if (noBail) {
+		compiler.plugin('this-compilation', function (compilation) {
+			compilation.plugin('additional-assets', function () {
+				this.callback(compilation.fileDependencies || [], compiler.options);
+				// We just want to get dependencies.
+				process.exit(0);
+			}.bind(this));
+		}.bind(this));
+	}
+
+	compiler.plugin('emit', function (compilation, compileCallback) {
+		this.callback(compilation.fileDependencies || [], compiler.options);
+		compileCallback();
+	}.bind(this));
 }
